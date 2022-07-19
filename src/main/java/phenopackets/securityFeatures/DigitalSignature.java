8b4887168120442311043d8abb75882d5632edfb
Judit-cab <judit.caballero.moro@estudiantat.upc.edu>
2022-07-19 21:00:59 +0200

    New digital signature methods

--- src/main/java/phenopackets/securityFeatures/DigitalSignature.java
+++ src/main/java/phenopackets/securityFeatures/DigitalSignature.java
@@ -1,97 +1,99 @@
 package phenopackets.securityFeatures;
-import static java.nio.charset.StandardCharsets.UTF_8;
 
 import com.google.crypto.tink.CleartextKeysetHandle;
 import com.google.crypto.tink.JsonKeysetReader;
+import com.google.crypto.tink.JsonKeysetWriter;
+import com.google.crypto.tink.KeyTemplates;
 import com.google.crypto.tink.KeysetHandle;
 import com.google.crypto.tink.PublicKeySign;
 import com.google.crypto.tink.PublicKeyVerify;
+
 import com.google.crypto.tink.signature.SignatureConfig;
-import com.google.crypto.tink.subtle.Hex;
+
+import com.google.gson.JsonObject;
 
 import java.io.File;
-import java.io.FileOutputStream;
+
 import java.io.IOException;
-
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
-import java.util.List;
-
+import java.util.Base64;
 
 
 public class DigitalSignature {
+
+    private static final String PK_FILE = "pk.json"; 
+    public static JsonObject jsonObj = new JsonObject();
     
-    public static void googleExample(String[] args) throws Exception {
-        if (args.length != 4) {
-            System.err.printf("Expected 4 parameters, got %d\n", args.length);
-            System.exit(1);
+    /**
+     *  Stores the private keyset in the projects resources/keysets directory if it does not exist yet.
+     * @throws IOException              Failure during saving
+     * @throws GeneralSecurityException Failure during keyset generation
+     * */
+    private static void createKeySet() throws IOException, GeneralSecurityException {
+        File keysetFile = new File(PK_FILE);
+        if (!keysetFile.exists()) {
+            KeysetHandle keysetHandle = KeysetHandle.generateNew(KeyTemplates.get("ECDSA"));
+            CleartextKeysetHandle.write(keysetHandle, JsonKeysetWriter.withFile(keysetFile));
         }
+    }
 
-        String mode = args[0];
-        if (!mode.equals("sign") && !mode.equals("verify")) {
-        System.err.println("Incorrect mode. Please select sign or verify.");
-        System.exit(1);
-        }
-        File keyFile = new File(args[1]);
-        byte[] msg = Files.readAllBytes(Paths.get(args[2]));
-        File signatureFile = new File(args[3]);
-
-        // Register all signature key types with the Tink runtime.
-        SignatureConfig.register();
-
-        // Read the keyset into a KeysetHandle.
-        KeysetHandle handle = null;
-        try {
-        handle = CleartextKeysetHandle.read(JsonKeysetReader.withFile(keyFile));
-        } catch (GeneralSecurityException | IOException ex) {
-        System.err.println("Cannot read keyset, got error: " + ex);
-        System.exit(1);
-        }
-
-        if (mode.equals("sign")) {
-        // Get the primitive.
+    private static byte[] signElement(byte[] element) throws GeneralSecurityException, IOException{
+        createKeySet();
+        KeysetHandle handle = CleartextKeysetHandle.read(JsonKeysetReader.withFile(new File(PK_FILE)));
         PublicKeySign signer = null;
-        try {
+
+        try{
             signer = handle.getPrimitive(PublicKeySign.class);
-        } catch (GeneralSecurityException ex) {
-            System.err.println("Cannot create primitive, got error: " + ex);
+        }catch(GeneralSecurityException ex){
+            System.err.println("Cannot create primitive: " + ex);
             System.exit(1);
         }
 
-        // Use the primitive to sign data.
-        byte[] signature = signer.sign(msg);
-        try (FileOutputStream stream = new FileOutputStream(signatureFile)) {
-            stream.write(Hex.encode(signature).getBytes(UTF_8));
-        }
-        System.exit(0);
-        }
+        // use primitive to sign
+        byte[] signature = signer.sign(element);
 
-        List<String> lines = Files.readAllLines(signatureFile.toPath());
-        if (lines.size() != 1) {
-        System.err.printf("The signature file should contain only one line,  got %d", lines.size());
-        System.exit(1);
-        }
-        byte[] signature = Hex.decode(lines.get(0).trim());
+        return signature;
+    }
 
-        // Get the primitive.
+    private static void verifyElement(byte[] element, byte[]signature) throws GeneralSecurityException, IOException{
+        createKeySet();
+        KeysetHandle handle = CleartextKeysetHandle.read(JsonKeysetReader.withFile(new File(PK_FILE)));
+
         PublicKeyVerify verifier = null;
-        try {
-        verifier = handle.getPrimitive(PublicKeyVerify.class);
+
+        try{
+            verifier = handle.getPrimitive(PublicKeyVerify.class);
         } catch (GeneralSecurityException ex) {
-        System.err.println("Cannot create primitive, got error: " + ex);
-        System.exit(1);
+            System.err.println("Cannot create primitive: " + ex);
         }
 
-        // Use the primitive to verify data.
-        try {
-        verifier.verify(signature, msg);
-        } catch (GeneralSecurityException ex) {
-        System.err.println("Signature verification failed.");
-        System.exit(1);
+        try{
+            verifier.verify(signature, element);
+        }catch (GeneralSecurityException ex) {
+            System.err.println("Signature verification failed.");
         }
+    }
+    
+    public static void protectWithDS(String mode, byte[] elementBytes, String elementName) throws IOException{
+        
+        try {
+            SignatureConfig.register();
+            
+            if(mode.equals("sign")){
+                byte[] signatureBytes = signElement(elementBytes);
+                //save the signature in a jsonObj
+                String ptSignature = new String(Base64.getEncoder().encode(signatureBytes), StandardCharsets.UTF_8);
+                jsonObj.addProperty(elementName, ptSignature);
 
-        System.exit(0);
+            }else if(mode.equals("verify")){
+                String ptSignature = jsonObj.getAsJsonArray("elementName").getAsString();
+                byte[] signatureBytes = Base64.getDecoder().decode(ptSignature);
+                verifyElement(elementBytes, signatureBytes);
+            }
+        }catch (java.security.GeneralSecurityException e){
+            System.out.println("Error protecting with DS");
+        }
     }
 
     private DigitalSignature() {}
